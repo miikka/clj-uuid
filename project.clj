81cd08dde283a63f4cbd452c885f37f7090700f4
Miikka Koskinen <miikka@metosin.fi>
2017-02-04 15:52:21 +0200

    Set project name to org.clojars.miikka/clj-uuid

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject danlentz/clj-uuid "0.1.7-SNAPSHOT"
+(defproject org.clojars.miikka/clj-uuid "0.1.7-SNAPSHOT"
   :description  "A Clojure library for generation and utilization of
                 UUIDs (Universally Unique Identifiers) as described by
                 RFC-4122. This library extends the standard Java UUID class
@@ -7,7 +7,7 @@
                 supporting utilities are provided to support serialization
                 and manipulation of these UUIDs in a simple, efficient manner."
   :author       "Dan Lentz"
-  :signing  {:gpg-key "B110BC3D"}
+  :deploy-repositories [["releases" :clojars]]
   :url          "https://github.com/danlentz/clj-uuid"
   :license      {:name "Eclipse Public License"
                   :url "http://www.eclipse.org/legal/epl-v10.html"}
